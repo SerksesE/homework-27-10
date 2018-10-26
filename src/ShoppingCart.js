b5c01b0b6fa72d102c95c1574576eb68a55d1c59
SerksesE <serkses@hotmail.com>
2018-10-26 19:37:02 +0200

    struggle

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,7 +1,33 @@
 class ShoppingCart {
-  constructor () {
+  constructor(name, quantity, pricePerUnit) {
 
+    this.items = []
+    this.name = name
+    this.quantity = quantity
+    this.pricePerUnit = pricePerUnit
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem() {
+    return this.items.push({
+      name: this.name,
+      quantity: this.quantity,
+      pricePerUnit: this.pricePerUnit
+    });
+
+  }
+  clear() {
+    this.items = [];
+  }
+  total(items) {
+    return items
+      .map(item => item.pricePerUnit * item.quantity)
+      .reduce((acc, item) => acc + item)
   }
 }
 
+
+
 module.exports = ShoppingCart
+
