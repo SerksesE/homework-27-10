9f43acea4d43997a52979daf67ae17d896d02510
SerksesE <serkses@hotmail.com>
2018-10-27 21:25:12 +0200

    almost done

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,24 +2,28 @@
 class ShoppingCart {
   constructor() {
     this.items = []
+  };
 
-  }
-
-  //
+//the get items method returns the items array
   getItems() {
     return this.items;
   }
 
+//the add item method pushes new objects the the items array
   addItem(name, quantity, pricePerUnit) {
     return this.items.push({
       name, quantity, pricePerUnit
     });
   }
 
+//the clear method returns an empty items array  
   clear() {
     return this.items = [];
   }
 
+//the total method returns the total price of the items in the items array
+//first it maps the price and multiplies it with the quantity
+//then it reduces all values to a single one 
   total() {
     return this.items
       .map(item => item.pricePerUnit * item.quantity)
@@ -27,6 +31,7 @@ class ShoppingCart {
   };
 };
 
+//exports the ShoppingCart class
 module.exports = ShoppingCart
 
 
--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,55 @@
-module.exports.groupAdultsByAgeRange = function () {
+module.exports.groupAdultsByAgeRange = function (array) {
+  //puts the array into a new variable
+  const array1 = array
 
-}
+  //adds different age groups
+  array1.forEach((age => {
+    if (age.age <= 20) {
+      age.ageGroup = "20 and younger"
+    } else if (age.age > 20 && age.age <= 30) {
+      age.ageGroup = "21-30"
+    } else if (age.age > 30 && age.age <= 40) {
+      age.ageGroup = "31-40"
+    } else if (age.age > 40 && age.age <= 50) {
+      age.ageGroup = "41-50"
+    } else if (age.age > 50) {
+      age.ageGroup = "51 and older"
+    }
+  }));
+
+  //check the result after adding the age group
+  //console.log(array1);
+
+  //create new array that holds objects
+  const array2 = {}
+
+  //creates a copy of the original array
+  array1.forEach(person => {
+
+    //checks if the array already exists in array2 
+    array2[person.ageGroup] ?
+
+      //if the array already exsist pushes a person in an age group category
+      array2[person.ageGroup].push({ name: person.name, age: person.age })
+
+      //creates a new array to put the age group and people in then pushes the first age group and person to it
+      : (array2[person.ageGroup] = [], array2[person.ageGroup].push({ name: person.name, age: person.age }));
+  });
+
+  //check the content of array2
+  //console.log(array2);
+
+  //deletes the added age group property from the original array
+  array1.forEach((age) => { delete age.ageGroup });
+
+  //check if array1 = array is back to the original state
+  //console.log(array1);
+
+  //returns array2 which has the right layout
+  return array2;
+  
+};
+
+
+
+
